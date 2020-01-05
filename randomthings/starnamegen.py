ad1a5b2703a2107b9008b8d4dc6fd7149749490b
Project-Sixth <six@inferno.space>
2020-01-05 03:27:10 +0300

    Fixed Upper to Capitalize

--- randomthings/starnamegen.py
+++ randomthings/starnamegen.py
@@ -10,7 +10,7 @@ def generate_starname_english():
     Part4 = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z","br","cr","dr","gr","kr","pr","sr","tr","str","vr","zr","bl","cl","fl","hl","gl","kl","ml","nl","pl","sl","tl","vl","zl","ch","sh","ph","th","bd","cd","gd","kd","ld","md","nd","pd","rd","sd","zd","bs","cs","ds","gs","ks","ls","ms","ns","ps","rs","ts","ct","gt","lt","nt","st","rt","zt","bb","cc","dd","gg","kk","ll","mm","nn","pp","rr","ss","tt","zz"]
     Part5 = ["","","","","","","","","","","","","","b","c","d","f","g","h","k","l","m","n","p","r","s","t","x","y","b","c","d","f","g","h","k","l","m","n","p","r","s","t","x","y","cs","ks","ls","ms","ns","ps","rs","ts","ys","ct","ft","kt","lt","nt","ph","sh","th"]
     if choice([1, 2]) == 1:
-        R = choice(Part1).upper() + choice(Part2) + choice(Part3) + choice(Part5)
+        R = choice(Part1) + choice(Part2) + choice(Part3) + choice(Part5)
     else:
-        R = choice(Part1).upper() + choice(Part2) + choice(Part3) + choice(Part4) + choice(Part3) + choice(Part5)
-    return R
+        R = choice(Part1) + choice(Part2) + choice(Part3) + choice(Part4) + choice(Part3) + choice(Part5)
+    return R.capitalize()
