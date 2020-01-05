9a32a08fdc0b254f06f4c01ecb598eb743d20408
Project-Sixth <six@inferno.space>
2020-01-05 12:51:09 +0300

    Fixed consontants and vowels in minlai names generation. Should be much plausible now!

--- randomthings/minlai.py
+++ randomthings/minlai.py
@@ -1,25 +1,45 @@
 from random import choice
-
-def generate_dayplopyname_russian():
-    S = ['б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ']
-    G = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+# Vowel and Consonant
+def generate_dayplopynamepart_russian():
+    AllConsontants = ['б', 'в', 'г', 'д', 'ж', 'з', 'л', 'м', 'н', 'р', 'б', 'в', 'г', 'д', 'ж', 'з', 'л', 'м', 'н', 'р', 'б', 'в', 'г', 'д', 'ж', 'з', 'л', 'м', 'н', 'р', 'й', 'к', 'п', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш']
+    AllVowels = ['а', 'и', 'о', 'у', 'е', 'а','и', 'о', 'у', 'е', 'а', 'и', 'о', 'у', 'е', 'а', 'и', 'о', 'у', 'е', 'э', 'ы', 'ё', 'ю', 'я']
     if choice([2, 3]) == 2:
         if choice([1, 2]) == 1:
-            R = choice(S).upper() + choice(G)
+            R = choice(AllConsontants) + choice(AllVowels)
         else:
-            R = choice(G).upper() + choice(S)
+            R = choice(AllVowels) + choice(AllConsontants)
     else:
-        R = choice(S).upper() + choice(G) + choice(S)
+        R = choice(AllConsontants) + choice(AllVowels) + choice(AllConsontants)
+    return R
+
+def generate_dayplopynamepart_english():
+    AllConsontants = ['b', 'd', 'g', 'j', 'k', 'l', 'm', 'n', 'q', 'r', 'v', 'w', 'b', 'd', 'g', 'j', 'k', 'l', 'm', 'n', 'q', 'r', 'v', 'w', 'c', 'f', 'h', 'p', 's', 't', 'x', 'y', 'z']
+    AllVowels = ['a', 'e', 'i', 'o', 'u']
+    if choice([2, 3]) == 2:
+        if choice([1, 2]) == 1:
+            R = choice(AllConsontants) + choice(AllVowels)
+        else:
+            R = choice(AllVowels) + choice(AllConsontants)
+    else:
+        R = choice(AllConsontants) + choice(AllVowels) + choice(AllConsontants)
+    return R
+
+def generate_dayplopyname_russian():
+    R = generate_dayplopynamepart_russian().capitalize()
     return R+"-"+R
     
 def generate_dayplopyname_english():
-    S = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-    G = ['a', 'e', 'i', 'o', 'u']
-    if choice([2, 3]) == 2:
-        if choice([1, 2]) == 1:
-            R = choice(S).upper() + choice(G)
-        else:
-            R = choice(G).upper() + choice(S)
-    else:
-        R = choice(S).upper() + choice(G) + choice(S)
+    R = generate_dayplopynamepart_english().capitalize()
     return R+"-"+R
+
+def generate_shadowdayplopyname_russian():
+    A = generate_dayplopynamepart_russian()
+    A += A[::-1]
+    R = A.capitalize() + "-" + generate_dayplopynamepart_russian().capitalize()
+    return R
+
+def generate_shadowdayplopyname_english():
+    A = generate_dayplopynamepart_english()
+    A += A[::-1]
+    R = A.capitalize() + "-" + generate_dayplopynamepart_english().capitalize()
+    return R
--- randomthings/randomthings.py
+++ randomthings/randomthings.py
@@ -58,6 +58,22 @@ class RandomThings(commands.Cog):
         Day Plopies have name consisting of 2-3 letters, forming a syllable, that is repeated 2 times, divided by dash.
         """
         await ctx.send("Random name of Day Plopy: {}.".format(generate_dayplopyname_english()))
+    
+    @randomthings.command()
+    async def shadowdayplopyname(self, ctx):
+        """
+        Создать имя случайного Тень-Дневного Плопи на русском языке.
+        Имена Дневных Плопи состоят из 2-3 букв, формирующих слог, который повторяется сам с собой через дефис.
+        """
+        await ctx.send("Случайное имя Тень-Дневного Плопи: {}.".format(generate_shadowdayplopyname_russian()))
+
+    @randomthings.command()
+    async def shadowdayplopynameen(self, ctx):
+        """
+        Create a random Shadow-Day Plopy name. Using ENglish language.
+        Day Plopies have name consisting of 2-3 letters, forming a syllable, that is repeated 2 times, divided by dash.
+        """
+        await ctx.send("Random name of Shadow-Day Plopy: {}.".format(generate_shadowdayplopyname_english()))
 
     @randomthings.command()
     async def nightplopyname(self, ctx):
