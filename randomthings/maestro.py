6afbf6803f8fb8bfbd5f1efe53faf4a72c92a8cf
Project-Sixth <six@inferno.space>
2020-05-29 15:51:35 +0300

    Code rewrite. Test one.

---
+++ randomthings/maestro.py
@@ -0,0 +1,73 @@
+import re
+import random
+import yaml
+
+config = {}
+
+### ORDER OF YAML FILE - first comes pool, then all subgens
+# pool:
+#   - Something reads that {{ generator }} is very rare, found only in {{ random_gen }} people
+#   - Another {{ random_gen }} is coming
+# vars (optional):
+#   sesso: "{{ generator }}"
+# generator:
+#   - white flower
+#   - black dye
+#   - "{{ random_gen }} witch"
+# random_gen:
+#   - drunken
+#   - erect
+#   - holding {{ sesso }}
+
+def check_and_define_vars():
+    global config
+    if 'vars' in config:
+        for var in config['vars']:
+            config['vars'][var] = mutate([config['vars'][var]])
+
+def getMutationFromConfig(match):
+    global config
+    
+    if match.group(1) in config['vars']:
+        return config['vars'][match.group(1)]
+    elif match.group(1) in config:
+        return random.choice(config[match.group(1)])
+    else:
+        raise Exception(f'No dictionary of variable "{match.group(1)}" found.')
+
+def getMutationFromFile(match):
+    try:
+        with open(f'./libs/{match.group(1)}.yml', 'r', encoding='utf-8') as f:
+            lib = yaml.safe_load(f)
+            return random.choice(lib)
+    except:
+        raise Exception(f'No file "libs/{match.group(1)}.yml" found.')
+
+def mutate(mutationTrail=[]):
+    if len(mutationTrail) > 1:
+        if mutationTrail[-2] == mutationTrail[-1]:
+            return mutationTrail[-1]
+    
+    newMutation = re.sub('{{ (.*?) }}', getMutationFromConfig, mutationTrail[-1], count=1)
+    newMutation = re.sub('\[\[ (.*?) \]\]', getMutationFromFile, newMutation, count=1)
+    
+    return mutate(mutationTrail + [newMutation])
+
+def load(gen_name):
+    global config
+
+    try:
+        with open(f'./gens/{gen_name}.yml', 'r', encoding='utf-8') as f:
+            config = yaml.safe_load(f)
+    except Exception as E:
+        return f'**Маэстро:** Ох! Кажется, произошла какая-то серьезная ошибка!\n```\nError of Reading-State:\n{E}\n```'
+       
+    try:
+        check_and_define_vars()
+    except Exception as E:
+        return f'**Маэстро:** Ох! Кажется, произошла какая-то серьезная ошибка!\n```\nError of Var-Defining-State:\n{E}\n```'
+    
+    return mutate([random.choice(config['pool'])])
+
+def say(message):
+    return mutate([message])
--- randomthings/randomthings.py
+++ randomthings/randomthings.py
@@ -2,128 +2,155 @@
 Project: Sixth module for doing random stuff
 """
 
-from random import choice
 from redbot.core import commands
 
-from .generators.minlaigen import generate_dayplopyname_russian, generate_dayplopyname_english, generate_shadowdayplopyname_russian, generate_shadowdayplopyname_english
-from .generators.starnamegen import generate_starname_russian, generate_starname_english
-from .generators.planetnamegen import generate_planetname_russian, generate_planetname_english
-from .generators.sylphnamegen import generate_sylphname_russian, generate_sylphname_english
-from .maestro.maestro import maestro_replace
+# from .generators.minlaigen import generate_dayplopyname_russian, generate_dayplopyname_english, generate_shadowdayplopyname_russian, generate_shadowdayplopyname_english
+# from .generators.starnamegen import generate_starname_russian, generate_starname_english
+# from .generators.planetnamegen import generate_planetname_russian, generate_planetname_english
+# from .generators.sylphnamegen import generate_sylphname_russian, generate_sylphname_english
+import maestro as MAE
 
 class RandomThings(commands.Cog):
     """
-    DnD game-like functions just for you!
+    A random things... a lot of them - just for you.
     """
 
     @commands.group()
-    async def randomthings(self, ctx):
+    async def maestro(self, ctx):
         """
-        All random generators from Random Things is here!
+        The Great One. The Mad One. The Best One.
         """
         pass
 
-    @randomthings.command()
-    async def starname(self, ctx):
+    @maestro.command()
+    async def say(self, ctx, text):
         """
-        Создать название случайной звезды на русском языке.
+        Make an improvised text, still, random. / Перечислить все доступные YML-файлы сценариев.
         """
-        await ctx.send("Случайная звезда: {}.".format(generate_starname_russian()))
+        ctx.send(MAE.say(text))
 
-    @randomthings.command()
-    async def starnameen(self, ctx):
+    @maestro.command()
+    async def load(self, ctx, scenario_name):
         """
-        Create a random star name on English Language
+        Load a YML-file of scenario. / Загрузить YML-файл сценария.
         """
-        await ctx.send("Random star name: {}.".format(generate_starname_english()))
+        ctx.send(MAE.load(scenario_name))
+    
+    @maestro.command()
+    async def list_scenarios(self, ctx):
+        """
+        List all available YML-scenarios. / Перечислить все доступные YML-файлы сценариев.
+        """
+        pass
+    
+    @maestro.command()
+    async def list_libraries(self, ctx):
+        """
+        List all available YML-libraries. / Перечислить все доступные YML-файлы библиотек.
+        """
+        pass
 
-    @randomthings.command()
-    async def planetname(self, ctx):
-        """
-        Создать название случайной планеты на русском языке.
-        """
-        await ctx.send("Случайная планета: {}.".format(generate_planetname_russian()))
+#     @randomthings.command()
+#     async def starname(self, ctx):
+#         """
+#         Создать название случайной звезды на русском языке.
+#         """
+#         await ctx.send("Случайная звезда: {}.".format(generate_starname_russian()))
 
-    @randomthings.command()
-    async def planetnameen(self, ctx):
-        """
-        Create a random planet name on English Language
-        """
-        await ctx.send("Random planet name: {}.".format(generate_planetname_english()))
+#     @randomthings.command()
+#     async def starnameen(self, ctx):
+#         """
+#         Create a random star name on English Language
+#         """
+#         await ctx.send("Random star name: {}.".format(generate_starname_english()))
 
-    @randomthings.command()
-    async def sylphname(self, ctx):
-        """
-        Создать название случайного сильфа на русском языке.
-        """
-        await ctx.send("Случайный сильф: {}.".format(generate_sylphname_russian()))
+#     @randomthings.command()
+#     async def planetname(self, ctx):
+#         """
+#         Создать название случайной планеты на русском языке.
+#         """
+#         await ctx.send("Случайная планета: {}.".format(generate_planetname_russian()))
 
-    @randomthings.command()
-    async def sylphnameen(self, ctx):
-        """
-        Create a random sylph name on English Language
-        """
-        await ctx.send("Random sylph name: {}.".format(generate_sylphname_english()))
+#     @randomthings.command()
+#     async def planetnameen(self, ctx):
+#         """
+#         Create a random planet name on English Language
+#         """
+#         await ctx.send("Random planet name: {}.".format(generate_planetname_english()))
 
-    @randomthings.command()
-    async def dayplopyname(self, ctx):
-        """
-        Создать имя случайного Дневного Плопи на русском языке.
-        Имена Дневных Плопи состоят из 2-3 букв, формирующих слог,
-        который повторяется сам с собой через дефис.
-        """
-        await ctx.send("Случайное имя Дневного Плопи: {}.".format(generate_dayplopyname_russian()))
+#     @randomthings.command()
+#     async def sylphname(self, ctx):
+#         """
+#         Создать название случайного сильфа на русском языке.
+#         """
+#         await ctx.send("Случайный сильф: {}.".format(generate_sylphname_russian()))
 
-    @randomthings.command()
-    async def dayplopynameen(self, ctx):
-        """
-        Create a random Day Plopy name. Using ENglish language.
-        Day Plopies have name consisting of 2-3 letters,
-        forming a syllable, that is repeated 2 times, divided by dash.
-        """
-        await ctx.send("Random name of Day Plopy: {}.".format(generate_dayplopyname_english()))
+#     @randomthings.command()
+#     async def sylphnameen(self, ctx):
+#         """
+#         Create a random sylph name on English Language
+#         """
+#         await ctx.send("Random sylph name: {}.".format(generate_sylphname_english()))
 
-    @randomthings.command()
-    async def shadowdayplopyname(self, ctx):
-        """
-        Создать имя случайного Тень-Дневного Плопи на русском языке.
-        Имена Дневных Плопи состоят из 2-3 букв, формирующих слог,
-        который повторяется сам с собой через дефис.
-        """
-        await ctx.send("Случайное имя Тень-Дневного Плопи: {}.".format(generate_shadowdayplopyname_russian()))
+#     @randomthings.command()
+#     async def dayplopyname(self, ctx):
+#         """
+#         Создать имя случайного Дневного Плопи на русском языке.
+#         Имена Дневных Плопи состоят из 2-3 букв, формирующих слог,
+#         который повторяется сам с собой через дефис.
+#         """
+#         await ctx.send("Случайное имя Дневного Плопи: {}.".format(generate_dayplopyname_russian()))
 
-    @randomthings.command()
-    async def shadowdayplopynameen(self, ctx):
-        """
-        Create a random Shadow-Day Plopy name. Using ENglish language.
-        Day Plopies have name consisting of 2-3 letters, forming a syllable,
-        that is repeated 2 times, divided by dash.
-        """
-        await ctx.send("Random name of Shadow-Day Plopy: {}.".format(generate_shadowdayplopyname_english()))
+#     @randomthings.command()
+#     async def dayplopynameen(self, ctx):
+#         """
+#         Create a random Day Plopy name. Using ENglish language.
+#         Day Plopies have name consisting of 2-3 letters,
+#         forming a syllable, that is repeated 2 times, divided by dash.
+#         """
+#         await ctx.send("Random name of Day Plopy: {}.".format(generate_dayplopyname_english()))
 
-    @randomthings.command()
-    async def nightplopyname(self, ctx):
-        """
-        Создать имя случайного Ночного Плопи на русском языке.
-        Ночные Плопи имеют имена, которые связаны с названиями небесных тел,
-        событий и космосом впринципе.
-        """
-        r = choice([generate_planetname_russian, generate_starname_russian, generate_sylphname_russian])
-        await ctx.send("Случайное имя Ночного Плопи: {}.".format(r()))
+#     @randomthings.command()
+#     async def shadowdayplopyname(self, ctx):
+#         """
+#         Создать имя случайного Тень-Дневного Плопи на русском языке.
+#         Имена Дневных Плопи состоят из 2-3 букв, формирующих слог,
+#         который повторяется сам с собой через дефис.
+#         """
+#         await ctx.send("Случайное имя Тень-Дневного Плопи: {}.".format(generate_shadowdayplopyname_russian()))
 
-    @randomthings.command()
-    async def nightplopynameen(self, ctx):
-        """
-        Create a random Night Plopy name. Using ENglish language.
-        Night Plopies usually have names that somehow connected to stars or cosmos.
-        """
-        r = choice([generate_planetname_english, generate_starname_english, generate_sylphname_english])
-        await ctx.send("Random name of Night Plopy: {}.".format(r()))
+#     @randomthings.command()
+#     async def shadowdayplopynameen(self, ctx):
+#         """
+#         Create a random Shadow-Day Plopy name. Using ENglish language.
+#         Day Plopies have name consisting of 2-3 letters, forming a syllable,
+#         that is repeated 2 times, divided by dash.
+#         """
+#         await ctx.send("Random name of Shadow-Day Plopy: {}.".format(generate_shadowdayplopyname_english()))
 
-# ====== MAESTRO ======
-    @randomthings.command()
-    async def maestro(self, ctx, *, msg: str):
-        """
-        Пробудить безумного Маэстро!
-        """
-        await ctx.send("**Маэстро**: {}".format(maestro_replace(msg)))
+#     @randomthings.command()
+#     async def nightplopyname(self, ctx):
+#         """
+#         Создать имя случайного Ночного Плопи на русском языке.
+#         Ночные Плопи имеют имена, которые связаны с названиями небесных тел,
+#         событий и космосом впринципе.
+#         """
+#         r = choice([generate_planetname_russian, generate_starname_russian, generate_sylphname_russian])
+#         await ctx.send("Случайное имя Ночного Плопи: {}.".format(r()))
+
+#     @randomthings.command()
+#     async def nightplopynameen(self, ctx):
+#         """
+#         Create a random Night Plopy name. Using ENglish language.
+#         Night Plopies usually have names that somehow connected to stars or cosmos.
+#         """
+#         r = choice([generate_planetname_english, generate_starname_english, generate_sylphname_english])
+#         await ctx.send("Random name of Night Plopy: {}.".format(r()))
+
+# # ====== MAESTRO ======
+#     @randomthings.command()
+#     async def maestro(self, ctx, *, msg: str):
+#         """
+#         Пробудить безумного Маэстро!
+#         """
+#         await ctx.send("**Маэстро**: {}".format(maestro_replace(msg)))
