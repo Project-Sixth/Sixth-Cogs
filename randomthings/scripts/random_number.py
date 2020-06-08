95b2f7e4a0ab38f9b78970724fc6c8d57119d2b4
Project-Sixth <six@inferno.space>
2020-06-08 13:53:54 +0300

    Fix stage 1

--- randomthings/scripts/random_number.py
+++ randomthings/scripts/random_number.py
@@ -1,15 +1,16 @@
 from random import randint
 
 def main(*args):
-    if len(args) == 0:
-        return str(randint(0, 100))
-    elif len(args) == 1:
-        try:
-            return str(randint(0, int(args[0])))
-        except:
-            return str(randint(0, 100))
-    else:
-        try:
-            return str(randint(int(args[0]), int(args[1])))
-        except:
-            return str(randint(0, 100))
+    return str(args)
+    # if len(args) == 0:
+    #     return str(randint(0, 100))
+    # elif len(args) == 1:
+    #     try:
+    #         return str(randint(0, int(args[0])))
+    #     except:
+    #         return str(randint(0, 100))
+    # else:
+    #     try:
+    #         return str(randint(int(args[0]), int(args[1])))
+    #     except:
+    #         return str(randint(0, 100))
