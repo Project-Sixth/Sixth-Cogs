39b18f9ea0737a05a361cebb656f9cea84bf283a
Project-Sixth <six@inferno.space>
2020-06-08 13:56:15 +0300

    Found error. Fixed.

--- randomthings/scripts/random_number.py
+++ randomthings/scripts/random_number.py
@@ -1,16 +1,15 @@
 from random import randint
 
 def main(*args):
-    return str(args)
-    # if len(args) == 0:
-    #     return str(randint(0, 100))
-    # elif len(args) == 1:
-    #     try:
-    #         return str(randint(0, int(args[0])))
-    #     except:
-    #         return str(randint(0, 100))
-    # else:
-    #     try:
-    #         return str(randint(int(args[0]), int(args[1])))
-    #     except:
-    #         return str(randint(0, 100))
+    if len(args) == 0:
+        return str(randint(0, 100))
+    elif len(args) == 1:
+        try:
+            return str(randint(0, int(args[0][0])))
+        except:
+            return str(randint(0, 100))
+    else:
+        try:
+            return str(randint(int(args[0][0]), int(args[0][1])))
+        except:
+            return str(randint(0, 100))
