0dffb0554892df8df7c11664045e248451ecff63
Project-Sixth <six@inferno.space>
2020-06-08 13:57:33 +0300

    Last fix. That is a trash. Eh..

--- randomthings/scripts/random_number.py
+++ randomthings/scripts/random_number.py
@@ -3,7 +3,7 @@ from random import randint
 def main(*args):
     if len(args) == 0:
         return str(randint(0, 100))
-    elif len(args) == 1:
+    elif len(args[0]) == 1:
         try:
             return str(randint(0, int(args[0][0])))
         except:
