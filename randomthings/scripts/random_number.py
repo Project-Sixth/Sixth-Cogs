cd56864078a0f91c5293e98df3e760ab5226db30
Project-Sixth <six@inferno.space>
2020-06-08 13:46:49 +0300

    Fixed 'int instead of str' error.

--- randomthings/scripts/random_number.py
+++ randomthings/scripts/random_number.py
@@ -2,14 +2,14 @@ from random import randint
 
 def main(*args):
     if len(args) == 0:
-        return randint(0, 100)
+        return str(randint(0, 100))
     elif len(args) == 1:
         try:
-            return randint(0, int(args(0)))
+            return str(randint(0, int(args(0))))
         except:
-            return randint(0, 100)
+            return str(randint(0, 100))
     else:
         try:
-            return randint(int(args(0)), int(args(1)))
+            return str(randint(int(args(0)), int(args(1))))
         except:
-            return randint(0, 100)
+            return str(randint(0, 100))
