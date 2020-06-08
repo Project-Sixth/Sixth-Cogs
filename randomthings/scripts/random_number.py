7d01ec52fc5733b686e953787f686a756ce2a991
Project-Sixth <six@inferno.space>
2020-06-08 13:50:04 +0300

    Dumb error. Now the int and str correctly fixed.

--- randomthings/scripts/random_number.py
+++ randomthings/scripts/random_number.py
@@ -5,11 +5,11 @@ def main(*args):
         return str(randint(0, 100))
     elif len(args) == 1:
         try:
-            return str(randint(0, int(args(0))))
+            return str(randint(0, int(args[0])))
         except:
             return str(randint(0, 100))
     else:
         try:
-            return str(randint(int(args(0)), int(args(1))))
+            return str(randint(int(args[0]), int(args[1])))
         except:
             return str(randint(0, 100))
